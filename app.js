d46c69d7e4a59f32af5a6c0094c276fbea0916ab
levi Lieberman <greggreg942@gmail.com>
2021-02-23 15:36:19 -0800

    css file served

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}...`);
+});
